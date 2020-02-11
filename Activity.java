97061e4fed61f5532016f921133c83d098528763
deepika-coder-lab <60639046+deepika-coder-lab@users.noreply.github.com>
2020-02-11 21:41:53 +0530

    Update Activity.java

--- Activity.java
+++ Activity.java
@@ -1,28 +1,49 @@
-package practise;
-
 public class Activity {
+	private String name;
+	private int startTime;
+	private int finishTime;
 
-	String name;
-	int startTime;
-	int endTime;
+	//Constructor
+	public Activity(String name, int startTime, int finishTime) {
+		this.name = name;
+		this.startTime = startTime;
+		this.finishTime = finishTime;
+	}//end of method
+
 	
 	public String getName() {
 		return name;
-	}
+	}//end of method
+
+	
 	public void setName(String name) {
 		this.name = name;
-	}
+	}//end of method
+
+	
 	public int getStartTime() {
 		return startTime;
-	}
+	}//end of method
+	
+
 	public void setStartTime(int startTime) {
 		this.startTime = startTime;
-	}
-	public int getEndTime() {
-		return endTime;
-	}
-	public void setEndTime(int endTime) {
-		this.endTime = endTime;
-	}
+	}//end of method
+
 	
-}
+	public int getFinishTime() {
+		return finishTime;
+	}//end of method
+
+	
+	public void setFinishTime(int finishTime) {
+		this.finishTime = finishTime;
+	}//end of method
+	
+
+	@Override
+	public String toString() {
+		return "Activity :" + name + ", start time = " + startTime + ", finish time = " + finishTime;
+	}//end of method
+
+}//end of class
